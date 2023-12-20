b319399261abad3ebc2f9a52a831bc91af636a8a
Andres <142694258+patoElDestripador@users.noreply.github.com>
2023-12-19 20:29:30 -0500

    a

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let lenguajeActual = ""
+
 function generarCards(cards){
     for (let index = 0; index < cards.length; index++) {
         if(index <= 3){
@@ -36,13 +38,51 @@ function generarCards(cards){
     }
 }
 
+function generarCardsDark(cards){
+    for (let index = 0; index < cards.length; index++) {
+        if(index <= 3){
+            document.getElementById("fila1").innerHTML += `
+            <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3 mt-3">
+            <div class="card h-100 bg-dark text-ligth " style="width: 18rem;">
+                <img src="${cards[index].img}" class="card-img-top" alt="...">
+                <div class="card-body text-light">
+                    <h6 class="card-subtitle">${cards[index].price}</h6>
+                    <h5 class="card-title">${cards[index].name}</h5>
+                    <p class="card-text">${cards[index].description}</p>
+                </div>
+                <div class="d-grid gap-2 card-footer bg-dark "> 
+                    <a href="#" class="btn btn-danger">Comprar</a>
+                </div>
+            </div>
+        </div>
+            `
+        }else{
+            document.getElementById("fila2").innerHTML += `
+            <div class="col-3 col-sm-12 col-md-6 col-lg-4 col-xl-3 mt-3">
+            <div class="card h-100 bg-dark text-ligth " style="width: 18rem;">
+                <img src="${cards[index].img}" class="card-img-top" alt="...">
+                <div class="card-body text-light">
+                    <h6 class="card-subtitle">${cards[index].price}</h6>
+                    <h5 class="card-title">${cards[index].name}</h5>
+                    <p class="card-text">${cards[index].description}</p>
+                </div>
+                <div class="d-grid gap-2 card-footer bg-dark "> 
+                    <a href="#" class="btn btn-danger">Comprar</a>
+                </div>
+            </div>
+        </div>
+            `
+        }
+    }
+}
+
+
 function cambiarColor(){
     let valor = document.getElementById("color").value
     let body = document.getElementById("bodry").classList
     let idioma = document.getElementById("idioma").classList
     let color = document.getElementById("color").classList
     let contenedor = document.getElementById("contenedor").classList
-    console.log(valor)
     if(valor == "dark"){
         body.replace("bg-light",`bg-${valor}`);
         body.replace("text-dark",`text-light`);
@@ -52,6 +92,9 @@ function cambiarColor(){
         idioma.replace("text-dark",`text-light`);
         contenedor.replace("bg-light",`bg-${valor}`);
         contenedor.replace("text-dark",`text-light`);
+        document.getElementById("fila1").innerHTML = ""
+        document.getElementById("fila2").innerHTML = ""
+        generarCardsDark(lenguajeActual)
     }else{
         body.replace("bg-dark",`bg-${valor}`);
         body.replace("text-light",`text-dark`);
@@ -61,6 +104,9 @@ function cambiarColor(){
         idioma.replace("text-light",`text-dark`);
         contenedor.replace("bg-dark",`bg-${valor}`);
         contenedor.replace("text-light",`text-dark`);
+        document.getElementById("fila1").innerHTML = ""
+        document.getElementById("fila2").innerHTML = ""
+        generarCards(lenguajeActual)
     }
 
 
@@ -74,23 +120,28 @@ function cambiarIdioma(){
     switch (parseInt(document.getElementById("idioma").value)) {
         case 1:
             console.log("entro a ingles",ingles.menu.burgers)
-            generarCards(ingles.menu.burgers);
+            lenguajeActual = ingles.menu.burgers
+            generarCards(lenguajeActual);
             break;
             case 0:
             console.log("entro a espñaol",espanol.menu.burgers)
-            generarCards(espanol.menu.burgers);
+            lenguajeActual = espanol.menu.burgers
+            generarCards(lenguajeActual);
             break;
             case 3:
             console.log("entro a fr",frances.menu.burgers)
-            generarCards(frances.menu.burgers);
+            lenguajeActual = frances.menu.burgers
+            generarCards(lenguajeActual);
             break;
             case 4:
             console.log("entro a jp",japones.menu.burgers)
-            generarCards(japones.menu.burgers);
+            lenguajeActual = japones.menu.burgers
+            generarCards(lenguajeActual);
             break;
 
         default:
-            generarCards(espanol.menu.burgers);
+            lenguajeActual = espanol.menu.burgers
+            generarCards(lenguajeActual);
             break;
     }
 }
